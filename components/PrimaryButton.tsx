4a4b1de2171fa0ffb15be0b3bf987dadd8f1c218
Favour Oghenekowho <gh.oghene@gmail.com>
2024-10-16 00:48:19 +0100

    init

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+export default function PrimaryButton({ children }: { children: string }) {
+  function handlePress() {
+    console.log("pressed");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={handlePress}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: "#8d143e",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+
+  buttonText: {
+    color: "white",
+  },
+});
---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    backgroundColor: "#4e0329",
+    elevation: 4,
+    shadowColor: "black",
+    shadowRadius: 6,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+  },
+
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
